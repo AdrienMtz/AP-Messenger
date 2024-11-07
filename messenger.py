94d3207df951294241875f04699112611889920f
Adrien Martinez <ad.mrtnz14@gmail.com>
2024-11-07 12:07:10 +0100

    quelques modifs

--- messenger.py
+++ messenger.py
@@ -1,23 +1,10 @@
 from datetime import datetime
+import json
 
-server = {
-    'users': [
-        {'id': 1, 'name': 'Alice'},
-        {'id': 2, 'name': 'Bob'}
-    ],
-    'channels': [
-        {'id': 1, 'name': 'Town square', 'member_ids': [1, 2]}
-    ],
-    'messages': [
-        {
-            'id': 1,
-            'reception_date': datetime.now(),
-            'sender_id': 1,
-            'channel': 1,
-            'content': 'Hi 👋'
-        }
-    ]
-}
+with open('C:/Users/Adrien/UE12/AP/Server-Messenger.json', 'r') as f :
+    server = json.load(f)
+
+# Fonctions annexes
 
 def id_to_name(str, k):
     L = []
@@ -25,12 +12,30 @@ def id_to_name(str, k):
         if object['id'] == k :
             L.append(object['name'])
     if len(L) == 0 :
-        print('No such user')
+        print('No such id')
     elif len(L) > 2 :
-        print(f'Error : 2 users for id {k}')
+        print(f'Error : 2 names for id {k}')
     else :
         return L[0]
 
+def name_to_id(str, name) :
+    L = []
+    for object in server[str]:
+        if object['name'] == name :
+            L.append(object['id'])
+    if len(L) == 0 :
+        print('No such name')
+    elif len(L) > 2 :
+        print(f'Error : 2 ids for name {name}')
+    else :
+        return L[0]
+
+def premier_indice(L,e) :
+    for i in range(len(L)) :
+        if L[i] == e :
+            return i
+
+# Fonctionnalités de Messenger
 
 def main_menu():
     print('\n')
@@ -45,15 +50,9 @@ def main_menu():
         see_channels()
     elif choice == 'c':
         print('\nBye !\n')
-        return None
+        save()
     else :
         print(f'Unknown option : {choice}')
-
-def premier_indice(L,e) :
-    for i in range(len(L)) :
-        if L[i] == e :
-            return i
-
 
 def see_users():
     print('\n')
@@ -102,12 +101,15 @@ def see_channels():
         print(f'Unknown option : {choice}')
         main_menu()
 
+def new_id(str):
+    return max(stuff['id'] for stuff in server[str]) + 1
+
 def create_user():
     name = input('Name : ')
-    id = max(user['id'] for user in server['users']) + 1
-    user = {'id' : id, 'name' : name}
+    user = {'id' : new_id('users'), 'name' : name}
     server['users'].append(user)
     print('New user created : ', user['id'], '. ', name, '\n')
+    save()
     print('a. See users')
     print('b. Back to main menu')
     print('\n')
@@ -122,17 +124,21 @@ def create_user():
 
 def see_messages(k):
     L = [m for m in server['messages'] if m['channel'] == k]
-    print('Channel ', k, ' : ', id_to_name('channels', k))
+    print('Channel ', k, ' : ', id_to_name('channels', k), '\n')
     for m in L :
         user = id_to_name('users', m['sender_id'])
-        print('(', m['reception_date'], ')', ' - ', user, ' : ',  m['content'])
+        print(f'({m["reception_date"]}) - {user} : {m["content"]}')
+    print('\n')
     print('a. See channels')
-    print('b. Back to main menu')
+    print('b. Write a message')
+    print('c. Back to main menu')
     print('\n')
     choice  = input('Select an option  : ')
     if choice == 'a' :
         see_users()
     elif choice == 'b' :
+        write_message(k)
+    elif choice == 'c' :
         main_menu()
     else :
         print(f'Unknown option : {choice}')
@@ -140,7 +146,7 @@ def see_messages(k):
     1  
 
 def create_channel():
-    ch = {'id' : len(server['channels']) +1, 'member_ids' : []}
+    ch = {'id' : new_id('channels'), 'member_ids' : []}
     ch['name'] = input('Channel name : ')
     choice = '0'
     while choice != 'n' :
@@ -152,6 +158,8 @@ def create_channel():
             print(f'Unknown option : {choice}')
             main_menu()
     server['channels'].append(ch)
+    print('New channel created')
+    save()
     see_channels()
 
 def add_user():
@@ -162,6 +170,7 @@ def add_user():
     else :
         server['channels'][ch_id-1]['member_ids'].append(user_id)
         print(f'User {user_id} has been added to channel {ch_id}')
+    save()
     see_channels()
 
 def remove_user():
@@ -172,9 +181,37 @@ def remove_user():
     else :
         server['channels'][ch_id-1]['member_ids'].pop(premier_indice(server['channels'][ch_id-1]['member_ids'], user_id))
         print(f'User {user_id} has been removed from channel {ch_id}')
+    save()
     see_channels()
 
+def save():
+    with open('C:/Users/Adrien/UE12/AP/Server-Messenger.json', 'w') as f :
+            json.dump(server, f)
+
+def write_message(k) :
+    print(f'Message to channel {k} : {id_to_name("channels",k)} \n')
+    name = input('Who writes ? \n')
+    user_id = name_to_id('users', name)
+    for channel in server['channels'] :
+        if channel['id'] == k :
+            ch = channel
+    if user_id not in ch['member_ids'] :
+        print(f'User {name} is not in channel {ch["id"]} : {ch["name"]}.')
+        choice = input('Do you want to add a new user to this channel ? (y/n)\n')
+        if choice == 'y' :
+            add_user()
+        elif choice == 'n' :
+            see_messages(k)
+        else :
+            print(f'Unknown option : {choice}')
+            main_menu()
+    else :
+        message = input('Message : ')
+        server['messages'].append({'id' : new_id('messages'), 'reception_date' : str(datetime.now()).split('.')[0], 'sender_id' : name_to_id('users', name), 'channel' : k, 'content' : message})
+        save()
+        see_messages(k)
 
 
 print('=== Messenger ===')
 main_menu()
+
