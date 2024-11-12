869e8a31ea958eddef42d77c5bac2d37bee9452d
Adrien Martinez <ad.mrtnz14@gmail.com>
2024-11-12 16:22:11 +0100

    Avant l'ajout des classes

--- messenger.py
+++ messenger.py
@@ -1,8 +1,33 @@
 from datetime import datetime
 import json
 
-with open('C:/Users/Adrien/UE12/AP/Server-Messenger.json', 'r') as f :
-    server = json.load(f)
+def open_server() :
+    with open('C:/Users/Adrien/UE12/AP/Server-Messenger.json', 'r') as f :
+        server = json.load(f)
+    return server
+
+class User :
+    def __init__(self, id : int, name : str) :
+        self.id = id
+        self.name = name
+
+class Channel :
+    def __init__(self, id : int, name : str, member_ids : list[int]) :
+        self.id = id
+        self.name = name
+        self.member_ids = member_ids
+
+class Message  :
+    def __init__(self, id : int, reception_date : str, sender_id : int, channel : int, content : str) :
+        self.id = id
+        self.reception_date = reception_date
+        self.sender_id = sender_id
+        self.channel = channel
+        self.content = content
+
+
+    
+
 
 # Fonctions annexes
 
